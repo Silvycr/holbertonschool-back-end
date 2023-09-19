dbbd25ae3b082f2f2dc2b0f1c089842a490874d0
Silvycr <silvia_alinacr@live.com>
2023-09-18 20:21:52 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,46 +1,17 @@
 #!/usr/bin/python3
 """ This module defines the REST API """
 import requests
-from sys import argv
-
-URL = 'https://jsonplaceholder.typicode.com'
-
-
-def get_name(id):
-    response = requests.get(f'{URL}/users/{id}')
-    response.raise_for_status()
-    user_data = response.json()
-    return user_data.get('name')
-
-
-def get_todos(id):
-    response = requests.get(f'{URL}/todos', params={'userId': id})
-    response.raise_for_status()
-    return response.json()
-
-
-def display_todo(id):
-    try:
-        employee_name = get_name(id)
-        todos = get_todos(id)
-
-        completed_tasks = [task for task in todos if task['completed']]
-        completed_count = len(completed_tasks)
-        total_tasks = len(todos)
-
-        print(f"Employee {employee_name} is done "
-              f"with tasks({completed_count}/{total_tasks}):")
-
-        for task in completed_tasks:
-            print(f"\t {task['title']}")
-
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-
+import sys
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        exit(1)
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = int(argv[1])
-    display_todo(employee_id)
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), completed_tasks, total_tasks))
+
+    [print(f"\t {todo['title']}") for todo in todos if todo["completed"]]
